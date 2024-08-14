fbea64377a481f56c36d7769f75c0cbe9d37d08c
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-08-13 23:09:55 -0700

    project setup and navbar

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/navbar/Navbar"
+
+function App() {
+
+  return (
+    <div className="container mx-auto">
+      <header>
+        <Navbar></Navbar>
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import { CgProfile } from "react-icons/cg";
+
+const Navbar = () => {
+    return (
+        <div className="navbar mt-2 md:mt-4">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li><a>Home</a></li>
+                        <li><a>Recipe</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">Recipe Calories</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li><a>Recipe</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Search</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end space-x-4">
+                <div className="form-control w-1/3 h-1/2 md:w-auto">
+                    <input type="text" placeholder="Search" className="input input-bordered rounded-full md:w-auto" />
+                </div>
+                <a className="btn text-xl rounded-full bg-green-400 font-semibold"><CgProfile /></a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
